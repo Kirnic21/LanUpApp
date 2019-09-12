dec4d09e329ee3dc629bc3f0109e18158b4e8896
Erick Andrade <erick-jp@hotmail.com>
2019-09-12 10:50:33 -0300

    feat: create component input label

---
+++ src/shared/components/InputLabel.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput
+} from 'react-native'
+
+export default InputLabel = ({ title }) => (
+  <View>
+    <View>
+      <Text style={{ color: "white", fontSize: 13 }}>{title}</Text>
+    </View>
+    <View>
+      <TouchableOpacity style={styles.TextInput} onPress={(event) => this.SelectedInput(event)}>
+        <TextInput
+          style={{ height: 45, width: 250 }}
+        />
+      </TouchableOpacity>
+    </View>
+  </View>
+)
+
+const styles = StyleSheet.create({
+  TextInput: {
+    borderColor: "white",
+    borderWidth: 1.5,
+    borderRadius: 50
+  },
+});
