72b6d7d00639ab2a2c84792abbba280011edf6b9
Bruno Meneses <brunosantosmeneses.16@hotmail.com>
2020-02-11 17:15:38 -0300

    style: adjustment title next event

---
+++ src/pages/NextEvent/TitleEvent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+import dimensions, { calcHeight, calcWidth } from "~/assets/Dimensions";
+
+const TitleEvent = ({ eventName, job, status }) => {
+  return (
+    <View style={styles.containerTitle}>
+      {status !== "without" ? (
+        <View>
+          <Text numberOfLines={1} style={styles.textTitle}>
+            {eventName}
+          </Text>
+          <Text style={styles.TextsubTitle}>{job}</Text>
+        </View>
+      ) : (
+        <View style={{}}>
+          <Text
+            numberOfLines={1}
+            style={[
+              styles.textTitle,
+              { textAlign: "center", paddingBottom: calcHeight(3) }
+            ]}
+          >
+            Sem evento
+          </Text>
+          <Text
+            style={{
+              color: "#FFF",
+              fontFamily: "HelveticaNowMicro-Regular",
+              fontSize: calcWidth(5),
+              textAlign: "center",
+
+              lineHeight: calcHeight(3)
+            }}
+          >
+            Você não possui nenhum{"\n"}próximo evento
+          </Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default TitleEvent;
