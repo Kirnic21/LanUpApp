16bab4266ad3d9a11aa3b809ddc41df9cd2d369c
Bruno Meneses <brunosantosmeneses.16@hotmail.com>
2020-02-20 14:37:34 -0300

    Merge

--- src/index.js
+++ src/index.js
@@ -22,8 +22,10 @@ class App extends Component {
   constructor(properties) {
     super(properties);
     OneSignal.init(ONE_SIGNAL_ID, {
-      kOSSettingsKeyInFocusDisplayOption: 2
+      kOSSettingsKeyInFocusDisplayOption: 0
     }); // set kOSSettingsKeyAutoPrompt to false prompting manually on iOS
+    // OneSignal.setLogLevel(6, 6);
+    OneSignal.inFocusDisplaying(0);
     OneSignal.addEventListener("received", this.onReceived);
     OneSignal.addEventListener("opened", this.onOpened);
     OneSignal.addEventListener("ids", this.onIds);
@@ -42,7 +44,7 @@ class App extends Component {
   onOpened(openResult) {
     // TODO: lógica de validação de tipo de push enviado
     console.log("Message: ", openResult.notification.payload.body);
-    console.log("Data: ", openResult.notification.payload.additionalData.type);
+    console.log("Data: ", openResult.notification.payload.additionalData);
     console.log("isActive: ", openResult.notification.isAppInFocus);
     console.log("openResult: ", openResult);
   }
