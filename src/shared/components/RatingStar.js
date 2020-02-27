ce707d9679fa78d2909faf5489b296f743f2b2e7
Bruno Meneses <brunosantosmeneses.16@hotmail.com>
2020-02-27 09:36:50 -0300

    feat: rating component

---
+++ src/shared/components/RatingStar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { View, Text } from "react-native";
+import StarRating from "react-native-star-rating";
+import { calcWidth } from "~/assets/Dimensions";
+
+const RatingStar = ({ title, rating, selectedStar }) => {
+  return (
+    <View>
+      <Text
+        style={{
+          // marginLeft: calcWidth(8),
+          fontFamily: "HelveticaNowMicro-Regular",
+          fontSize: calcWidth(3.5),
+          color: "#FFFFFF",
+          padding: calcWidth(1)
+        }}
+      >
+        {title}
+      </Text>
+      <View
+        style={{
+          backgroundColor: "#24203B",
+
+          padding: calcWidth(2.5),
+          paddingHorizontal: calcWidth(5),
+          borderRadius: calcWidth(15),
+          marginBottom: calcWidth(4)
+        }}
+      >
+        <StarRating
+          disabled={false}
+          maxStars={5}
+          emptyStar={"star"}
+          fullStar={"star"}
+          emptyStarColor="#C5B9EE"
+          fullStarColor="#FFCC00"
+          halfStar={"star-half"}
+          iconSet={"MaterialCommunityIcons"}
+          rating={rating}
+          starSize={calcWidth(14)}
+          selectedStar={selectedStar}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default RatingStar;
