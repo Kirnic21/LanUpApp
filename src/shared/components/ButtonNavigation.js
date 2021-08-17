a50d908c2cb29ea52ee9f09ce6ed48b1eb1fee0d
Bruno Meneses <bsantos@vntrx.com>
2021-08-17 17:38:05 -0300

    chore: change button menu

--- src/shared/components/ButtonNavigation.js
+++ src/shared/components/ButtonNavigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TouchableOpacity } from "react-native";
+import { View, TouchableOpacity, StyleSheet } from "react-native";
 import { DrawerActions } from "react-navigation-drawer";
 import { calcWidth } from "~/assets/Dimensions/index";
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -14,27 +14,38 @@ const ButtonNavigation = (props) => (
     ]}
   >
     <TouchableOpacity
+      style={props.type === "drawer" ? drawer : styles.button}
       onPress={() => {
         props.type === "drawer"
           ? props.navigation.dispatch(DrawerActions.openDrawer())
           : props.type === "stack"
           ? props.navigation.goBack()
-          : props.type === 'route'
+          : props.type === "route"
           ? props.navigation.push(props.nameRoute)
           : props.onPress();
       }}
     >
       <Icon
         color={"#FFF"}
-        name={props.type === "drawer" ? "short-text" : "chevron-left"}
+        name={props.type === "drawer" ? "menu" : "chevron-left"}
         size={calcWidth(8.5)}
-        style={{
-          borderColor: "#FFFFFF",
-          padding: calcWidth(0.5),
-        }}
       />
     </TouchableOpacity>
   </View>
 );
+
+const styles = StyleSheet.create({
+  button: {
+    borderColor: "#FFFFFF",
+    borderWidth: 1,
+    borderRadius: calcWidth(10),
+    padding: calcWidth(0.5),
+  },
+});
+
+const drawer = StyleSheet.compose(styles.button, {
+  backgroundColor: "#483D8B",
+  padding: calcWidth(1),
+});
 
 export default ButtonNavigation;
