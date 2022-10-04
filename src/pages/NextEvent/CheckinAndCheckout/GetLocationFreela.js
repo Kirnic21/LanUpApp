daa66db126ebaa6480e181298b6d9660766bc550
Bruno Meneses <bsantos@vntrx.com>
2022-10-04 15:02:52 -0300

    fix: get location checkin

--- src/pages/NextEvent/CheckinAndCheckout/GetLocationFreela.js
+++ src/pages/NextEvent/CheckinAndCheckout/GetLocationFreela.js
@@ -1,23 +1,16 @@
 import Geolocation from "react-native-geolocation-service";
 
 import { AlertHelper } from "~/shared/helpers/AlertHelper";
-
+import { hasLocationPermission } from "~/shared/helpers/PermissionGeolocation";
 import { checkpoints, location } from "~/shared/services/operations.http";
-import RequestPermission from "~/shared/helpers/PermissionGeolocation";
 
-const getLatLong = ({ origin, operationId: id, job }) =>
-  new Promise((resolve) => {
-    Geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => {
-        getLocationFreela(origin, id, job, latitude, longitude);
-        resolve(true);
-      },
-      (error) => {
-        RequestPermission(error);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  });
+const errors = {
+  1: "A permissão de localização não foi concedida",
+  2: "O provedor de localização não está disponível",
+  3: "A solicitação de localização expirou",
+  4: "O serviço Google Play não está instalado ou tem uma versão mais antiga",
+  5: "O serviço de localização não está ativado ou o modo de localização não é apropriado para a solicitação atual",
+};
 
 const getLocationFreela = async (origin, id, job, latitude, longitude) => {
   try {
@@ -36,6 +29,45 @@ const getLocationFreela = async (origin, id, job, latitude, longitude) => {
   } catch (error) {
     AlertHelper.show("error", "Erro", error.message);
   }
+};
+
+const getLatLong = async ({ origin, operationId: id, job }) => {
+  const hasPermission = await hasLocationPermission();
+
+  if (!hasPermission) {
+    stopBackground.stop();
+    return;
+  }
+
+  new Promise((resolve) => {
+    Geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        getLocationFreela(origin, id, job, latitude, longitude);
+        resolve(true);
+      },
+      (error) => {
+        AlertHelper.show(
+          "error",
+          "Erro",
+          `${
+            errors[error?.code] || "Ocorreu um erro, por favor tente mais tarde"
+          }`
+        );
+      },
+      {
+        accuracy: {
+          android: "high",
+          ios: "best",
+        },
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        forceRequestLocation: true,
+        forceLocationManager: false,
+        showLocationDialog: true,
+      }
+    );
+  });
 };
 
 export default getLatLong;
